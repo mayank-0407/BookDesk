722a7cb4cf2e796a013287da56b06b3d8c129000
Mayank Aggarwal <97796657+mayank-0407@users.noreply.github.com>
2023-01-27 21:58:10 +0530

    changed allowed host

--- bookdesk/settings.py
+++ bookdesk/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ["secret_key"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ["allowed_host"]]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
